b65829efd4c25d7eddbb7da81ca5721e995f2fc3
Varokas Panusuwan <varokas@panusuwan.com>
2017-07-01 18:04:10 +0700

    add jest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
+import _ from 'lodash';
 
-import { Button } from 'semantic-ui-react'
+import { Form, Container, Table, Header } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+
+const currentYear = new Date().getFullYear();
+const gainOptions = _.range(1,11).map( i => ({ key: i, text: `${i}%`, value: i }) )
+const savingYears = _.range(1,16);
 
 class App extends Component {
   render() {
     return (
-      <Button>
-        Click Here
-      </Button>
+      <Container>
+        <Header as='h1'>First Header</Header>
+        <Form>
+          <Form.Group widths='equal'>
+            <Form.Input label='จำนวนปี' control='input' type='number' value={15}/>
+            <Form.Select label='ผลตอบแทนต่อปี' options={gainOptions} value={2}/>
+            <Form.Input label='เงินต่อเดือน' control='input' type='number' value={20000}/>
+            <Form.Input label='ใช้ถึงปี' control='input' type='number' value={90} />
+          </Form.Group>
+        </Form>
+        <Header as='h2'>เก็บเงิน</Header>
+        <Table unstackable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>ปี</Table.HeaderCell>
+              <Table.HeaderCell>เงินเก็บต้นปี</Table.HeaderCell>
+              <Table.HeaderCell>เงินเก็บปลายปี</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            { savingYears.map( (y) => 
+              <Table.Row key={y}>
+                <Table.Cell>{y}</Table.Cell>
+                <Table.Cell>0</Table.Cell>
+                <Table.Cell>0</Table.Cell>
+              </Table.Row>
+              )
+            }
+          </Table.Body>
+        </Table>
+      </Container>
     );
   }
 }
---
+++ src/lib/calc.test.js
@@ -0,0 +1,5 @@
+import { square } from './calc';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(3).toBe(3);
+});
