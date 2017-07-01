d7c50b08c05f7b8df737b7b5ef6e62facf9108bb
Varokas Panusuwan <varokas@panusuwan.com>
2017-07-01 18:27:08 +0700

    add calc

--- src/lib/calc.js
+++ src/lib/calc.js
@@ -1,3 +1,19 @@
-export function square(x) {
-    return x * x;
+import _ from 'lodash';
+
+export function savings(years, savingPerMonth) {
+  let savingsPerYear = savingPerMonth * 12;
+
+  var result = []
+  var currentSavings = 0
+  
+  for(var i=1;i<=years;i++) {
+    
+    currentSavings += savingsPerYear;
+    result.push({
+        year: i, 
+        yearEnd: currentSavings
+    })
+  }
+
+  return result
 }
--- src/lib/calc.test.js
+++ src/lib/calc.test.js
@@ -1,5 +1,14 @@
-import { square } from './calc';
+import { savings } from './calc';
+import _ from 'lodash';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+test('savings() return correct years', () => {
+  let years = savings(15).map( i => i.year )
+
+  expect(years).toHaveLength(15)
+  expect(years).toEqual(_.range(1,16))
+});
+
+test('savings() return compounded savings without ', () => {
+  let result = savings(5, 10000)
+  expect(result[4].yearEnd).toEqual(10000 * 12 * 5)
 });
